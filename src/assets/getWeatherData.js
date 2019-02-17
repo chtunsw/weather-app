b15d588a7f60f98690e6289d2b371fbfe2013ef9
Chen Haotian <chtunsw@gmail.com>
2019-02-17 20:48:54 +1100

    handle api err

--- src/assets/getWeatherData.js
+++ src/assets/getWeatherData.js
@@ -1,8 +1,14 @@
 import axios from 'axios'
 
-async function getWeatherData (location) {
-    const weatherResponse = await axios.get(`http://api.apixu.com/v1/forecast.json?key=a027b8abca1f45809a594902192301&q=${location}&days=7`)
-    console.log(weatherResponse.data)
+async function getWeatherData(location) {
+    let weatherResponse = null
+    try {
+        weatherResponse = await axios.get(`http://api.apixu.com/v1/forecast.json?key=a027b8abca1f45809a594902192301&q=${location}&days=7`)
+        console.log(weatherResponse.data)
+    }
+    catch (err) {
+        console.log(err.message)
+    }
     return weatherResponse.data
 }
 
